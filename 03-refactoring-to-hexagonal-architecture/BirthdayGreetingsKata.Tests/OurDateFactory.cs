916ea57394be78c24d28ec05cfb5186af1a225cd
Joser Gala Naranjo <jgalanar@gmail.com>
2025-12-09 14:07:05 +0100

    refactor: inline variable

--- 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata.Tests/OurDateFactory.cs
+++ 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata.Tests/OurDateFactory.cs
@@ -6,7 +6,6 @@ public static class OurDateFactory
 {
     public static OurDate GetDateFromString(string stringDate)
     {
-        var date = DateTime.ParseExact(stringDate, "yyyy/MM/dd", null);
-        return new OurDate(date);
+        return new OurDate(DateTime.ParseExact(stringDate, "yyyy/MM/dd", null));
     }
 }
