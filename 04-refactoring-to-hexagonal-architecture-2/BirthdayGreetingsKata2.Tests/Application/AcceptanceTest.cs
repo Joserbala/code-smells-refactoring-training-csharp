811629b2a91312946f834b894717a5a1f31c67e0
Joser Gala Naranjo <jgalanar@gmail.com>
2025-12-10 13:40:43 +0100

    test: use mock instead of field

--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2.Tests/Application/AcceptanceTest.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2.Tests/Application/AcceptanceTest.cs
@@ -13,43 +13,20 @@ namespace BirthdayGreetingsKata2.Tests.Application;
 
 public class AcceptanceTest
 {
-    private const int SmtpPort = 25;
-    private const string SmtpHost = "localhost";
-    private const string From = "sender@here.com";
-    private List<MailMessage> _messagesSent;
     private BirthdayService _service;
     private readonly Employee _john = new("John", "Doe", OurDate("1982/10/08"), "john.doe@foobar.com");
     private readonly Employee _mary = new("Mary", "Ann", OurDate("1975/03/11"), "mary.ann@foobar.com");
-    private IGreetingSender _greetingServiceForTesting;
-    private const string EmployeesFilePath = "Application/employee_data.txt";
-
-    private class GreetingSenderForTesting : EmailGreetingSender
-    {
-        private readonly List<MailMessage> _messagesSent;
-
-        public GreetingSenderForTesting(List<MailMessage> messagesSent, string smtpHost, int smtpPort, string sender) :
-            base(smtpHost, smtpPort, sender)
-        {
-            _messagesSent = messagesSent;
-        }
-
-        protected override void Send(MailMessage msg)
-        {
-            _messagesSent.Add(msg);
-        }
-    }
+    private IGreetingSender _greetingSenderMock;
 
     [SetUp]
     public void SetUp()
     {
-        _messagesSent = new List<MailMessage>();
-        var messageSenderForTesting = new GreetingSenderForTesting(_messagesSent, SmtpHost, SmtpPort, From);
-        _greetingServiceForTesting = Substitute.For<IGreetingSender>();
+        _greetingSenderMock = Substitute.For<IGreetingSender>();
 
         var mock = Substitute.For<IEmployeesRepository>();
 
         mock.GetAll().Returns([_john, _mary]);
-        _service = new BirthdayService(mock, _greetingServiceForTesting);
+        _service = new BirthdayService(mock, _greetingSenderMock);
     }
 
     [Test]
@@ -59,8 +36,9 @@ public class AcceptanceTest
 
         _service.SendGreetings(today);
 
-        _greetingServiceForTesting.Received().Send(Arg.Is<List<GreetingMessage>>(x => x.Count == 1));
-        _greetingServiceForTesting.Received().Send(Arg.Is<List<GreetingMessage>>(x => x.Single().Equals(GreetingMessage.GenerateForSome(new List<Employee> {_john}).Single())));
+        _greetingSenderMock.Received().Send(Arg.Is<List<GreetingMessage>>(x => x.Count == 1));
+        _greetingSenderMock.Received().Send(Arg.Is<List<GreetingMessage>>(x =>
+            x.Single().Equals(GreetingMessage.GenerateForSome(new List<Employee> { _john }).Single())));
     }
 
     [Test]
@@ -70,6 +48,6 @@ public class AcceptanceTest
 
         _service.SendGreetings(today);
 
-        Assert.That(_messagesSent, Is.Empty);
+        _greetingSenderMock.Received().Send(Arg.Is<List<GreetingMessage>>(x => x.Count == 0));
     }
 }
