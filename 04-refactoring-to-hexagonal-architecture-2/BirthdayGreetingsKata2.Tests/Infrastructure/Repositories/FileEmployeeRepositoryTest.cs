ffe1e79758eb986b74c94e866fd40a674f733df9
Joser Gala Naranjo <jgalanar@gmail.com>
2025-12-10 12:42:41 +0100

    test: add happy path test

--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2.Tests/Infrastructure/Repositories/FileEmployeeRepositoryTest.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2.Tests/Infrastructure/Repositories/FileEmployeeRepositoryTest.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using BirthdayGreetingsKata2.Core;
 using BirthdayGreetingsKata2.Infrastructure.Repositories;
+using BirthdayGreetingsKata2.Tests.helpers;
 using NUnit.Framework;
 
 
@@ -7,6 +9,22 @@ namespace BirthdayGreetingsKata2.Tests.Infrastructure.Repositories;
 
 public class FileEmployeeRepositoryTest
 {
+    [Test]
+    public void Loads_Employees_With_Valid_Path()
+    {
+        IEmployeesRepository employeesRepository =
+            new FileEmployeesRepository("Infrastructure/Repositories/employee_data.txt");
+
+        var employees = employeesRepository.GetAll();
+        Assert.That(employees.Count, Is.EqualTo(2));
+
+        var john = new Employee("John", "Doe", OurDateFactory.OurDate("1982/10/08"), "john.doe@foobar.com");
+        Assert.That(employees.Any(e => e.Equals(john)));
+
+        var mary = new Employee("Mary", "Ann", OurDateFactory.OurDate("1975/03/11"), "mary.ann@foobar.com");
+        Assert.That(employees.Any(e => e.Equals(mary)));
+    }
+
     [Test]
     public void Fails_When_The_File_Does_Not_Exist()
     {
