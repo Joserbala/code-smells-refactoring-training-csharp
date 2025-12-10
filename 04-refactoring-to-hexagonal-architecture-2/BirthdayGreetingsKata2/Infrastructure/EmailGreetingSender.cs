345fed79e27adda0430b1f11415190fb30839c36
Joser Gala Naranjo <jgalanar@gmail.com>
2025-12-10 13:41:30 +0100

    refactor: convert to primary constructor

--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Infrastructure/EmailGreetingSender.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Infrastructure/EmailGreetingSender.cs
@@ -1,33 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Mail;
 using BirthdayGreetingsKata2.Core;
 
 namespace BirthdayGreetingsKata2.Infrastructure;
 
-public class EmailGreetingSender : IGreetingSender {
-    private readonly string _smtpHost;
-    private readonly int _smtpPort;
-    private readonly string _sender;
+public class EmailGreetingSender(string smtpHost, int smtpPort, string sender) : IGreetingSender
+{
+    public void Send(List<GreetingMessage> greetings) => greetings.Select(MailMessageFrom).ToList().ForEach(Send);
 
-    public EmailGreetingSender(string smtpHost, int smtpPort, string sender) {
-        _smtpHost = smtpHost;
-        _smtpPort = smtpPort;
-        _sender = sender;
-    }
-
-    public void Send(List<GreetingMessage> greetings) {
-        foreach (var greeting in greetings)
-        {
-            Send(MailMessageFrom(greeting));
-        }
-    }
-    
     // made protected for testing :-(
-    protected virtual void Send(MailMessage msg) {
-        new SmtpClient(_smtpHost, _smtpPort).Send(msg);
-    }
+    protected virtual void Send(MailMessage msg) => new SmtpClient(smtpHost, smtpPort).Send(msg);
 
-    private MailMessage MailMessageFrom(GreetingMessage greeting) {
-        return new MailMessage(_sender, greeting.To(), greeting.Subject(), greeting.Text());
-    }
+    private MailMessage MailMessageFrom(GreetingMessage greeting)
+        => new(sender, greeting.To(), greeting.Subject(), greeting.Text());
 }
