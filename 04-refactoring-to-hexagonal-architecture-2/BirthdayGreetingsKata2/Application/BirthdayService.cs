e4870b138965685c9e769bf409d067f91d031b31
Joser Gala Naranjo <jgalanar@gmail.com>
2025-12-10 10:42:33 +0100

    refactor: move SendMessage to EmailMessageSender

--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Application/BirthdayService.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Application/BirthdayService.cs
@@ -40,29 +40,7 @@ public class BirthdayService
             var recipient = message.To();
             var body = message.Text();
             var subject = message.Subject();
-            SendMessage(smtpHost, smtpPort, sender, subject, body, recipient);
+            _messageSender.SendMessage(smtpHost, smtpPort, sender, subject, body, recipient);
         }
-    }
-
-    private void SendMessage(string smtpHost, int smtpPort, string sender,
-        string subject, string body, string recipient)
-    {
-        // Create a mail session
-        var smtpClient = new SmtpClient(smtpHost)
-        {
-            Port = smtpPort
-        };
-
-        // Construct the message
-        var msg = new MailMessage
-        {
-            From = new MailAddress(sender),
-            Subject = subject,
-            Body = body
-        };
-        msg.To.Add(recipient);
-
-        // Send the message
-        _messageSender.SendMessage(msg, smtpClient);
     }
 }
--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Infrastructure/EmailMessageSender.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Infrastructure/EmailMessageSender.cs
@@ -9,4 +9,26 @@ public class EmailMessageSender
     {
         smtpClient.Send(msg);
     }
+
+    public void SendMessage(string smtpHost, int smtpPort, string sender,
+        string subject, string body, string recipient)
+    {
+        // Create a mail session
+        var smtpClient = new SmtpClient(smtpHost)
+        {
+            Port = smtpPort
+        };
+
+        // Construct the message
+        var msg = new MailMessage
+        {
+            From = new MailAddress(sender),
+            Subject = subject,
+            Body = body
+        };
+        msg.To.Add(recipient);
+
+        // Send the message
+        this.SendMessage(msg, smtpClient);
+    }
 }
